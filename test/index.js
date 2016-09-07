3ad934d9285484eabfcba2a3fcda6ea1c8748ecf
yabirgb <zanklord@gmail.com>
2016-09-07 13:28:35 +0200

    Initial commit version 1.0.0

---
+++ test/index.js
@@ -0,0 +1,67 @@
+var validUrl = require('../index.js');
+var assert = require('assert');
+
+var url = "google.com"
+assert.equal(validUrl(url), true);
+
+var url = "http://google.com"
+assert.equal(validUrl(url), true);
+
+var url = "https://google.com"
+assert.equal(validUrl(url), true);
+
+var url = "http://www.google.com"
+assert.equal(validUrl(url), true);
+
+var url = "https://www.google.com"
+assert.equal(validUrl(url), true);
+
+var url = "google.es"
+assert.equal(validUrl(url), true);
+
+var url = "http://google.es"
+assert.equal(validUrl(url), true);
+
+var url = "https://google.es"
+assert.equal(validUrl(url), true);
+
+var url = "https://www.google.com"
+assert.equal(validUrl(url), true);
+
+var url = "http://www.google.com"
+assert.equal(validUrl(url), true);
+
+var url = "https://d1f4470da51b49289906b3d6cbd65074@app.getsentry.com/13176"
+assert.equal(validUrl(url), true);
+
+var url = "d1f4470da51b49289906b3d6cbd65074@app.getsentry.com/13176"
+assert.equal(validUrl(url), true);
+
+var url = "mongodb://u:p@example.com:21332/mydb"
+assert.equal(validUrl(url), true);
+
+var url = "wss://secure.example.com/biz"
+assert.equal(validUrl(url), true);
+
+//Not valid
+
+var url = "google"
+assert.equal(validUrl(url), false);
+
+var url = "http://google"
+assert.equal(validUrl(url), false);
+
+var url = "https://google"
+assert.equal(validUrl(url), false);
+
+var url = "www.google"
+assert.equal(validUrl(url), false);
+
+var url = "http://www.google"
+assert.equal(validUrl(url), false);
+
+var url = "https://www.google"
+assert.equal(validUrl(url), false);
+
+var url = ""
+assert.equal(validUrl(url), false);
